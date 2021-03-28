7772755d4e9235036f1d048fffb675f474d3fa90
Dylancouzon <dylancouzon@gmail.com>
2021-03-28 14:54:33 -0700

    add timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 var startButton = document.querySelector("#start");
+var timerBox = document.getElementById("timer");
+var time = 31;
+
+
 
 startButton.addEventListener("click", function(event) {
+  // Timer
+  var timer = setInterval(function(answer){
+    timerBox.innerHTML = "";
+    time--;
+    timerBox.textContent = time + " seconds left.";
 
+    if(time === 0){
+      clearInterval(timer);
+      timerBox.innerHTML = "Game Over <button id='enterScore'>Enter Score</button>";
+    }
+  }, 1000);
+  
 });
