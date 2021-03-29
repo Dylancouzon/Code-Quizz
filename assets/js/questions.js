125c73f56bae49939fede83f82fea688c5f239f7
Dylancouzon <dylancouzon@gmail.com>
2021-03-28 17:42:15 -0700

    store score in Local storage

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -6,15 +6,87 @@ var questions = {
         b: "Bla bla",
         c: "Okuurrr",
         d: "HTML",
-        win: "b"
+        win: "a"
     },
 
     1:{
         question: "What does the cow says ?",
         a: "Meow",
         b: "Bla bla",
-        c: "Okuurrr",
+        c: "x",
         d: "Lol",
         win: "b"
+    },
+
+    2:{
+        question: "What does the dog says ?",
+        a: "Meow",
+        b: "Bla bla",
+        c: "Okuurrr",
+        d: "Lol",
+        win: "c"
+    },
+
+    3:{
+        question: "What does thawdasde cat says ?",
+        a: "Meow",
+        b: "b",
+        c: "Okuurrr",
+        d: "d",
+        win: "d"
+    },
+
+    4:{
+        question: "What doeqweasds the cat says ?",
+        a: "Meow",
+        b: "d",
+        c: "Okuurrr",
+        d: "c",
+        win: "d"
+    },
+
+    5:{
+        question: "What does wadawdxthe cat says ?",
+        a: "Meow",
+        b: "d",
+        c: "Okuurrr",
+        d: "Mevvow",
+        win: "d"
+    },
+
+    6:{
+        question: "Whasadasdt does the cat says ?",
+        a: "Meow",
+        b: "Meosdw",
+        c: "Okuurrr",
+        d: "Meaow",
+        win: "d"
+    },
+
+    7:{
+        question: "What does the cafft says ?",
+        a: "Meow",
+        b: "Meaow",
+        c: "Okuurrr",
+        d: "Mesdow",
+        win: "d"
+    },
+
+    8:{
+        question: "What does the cffat says ?",
+        a: "Meosw",
+        b: "Meodw",
+        c: "Okuurrr",
+        d: "Mddaeow",
+        win: "d"
+    },
+
+    9:{
+        question: "What does the cafft says ?",
+        a: "Measdow",
+        b: "Meow",
+        c: "Okuurrr",
+        d: "Meoddw",
+        win: "d"
     }
 };
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,51 +3,55 @@ var timerBox = document.getElementById("timer");
 var question = document.getElementById("question");
 var main = document.getElementById("main");
 var score = 0;
+var nextQuestionId = 0;
+var time;
+var storedScore = JSON.parse(localStorage.getItem("score"));
+if(!storedScore){var storedScore = [];}
 
 // Show next question and options 
-function nextQuestion(nextquestion) {
+function nextQuestion(nextQuestionId) {
 
-  question.innerHTML = questions[nextquestion].question;
-  console.log(questions[nextquestion].win);
-
+  question.innerHTML = questions[nextQuestionId].question;
+  main.innerHTML = "";
   var choice1 = document.createElement("a");
-  choice1.innerHTML = questions[nextquestion].a;
+  choice1.innerHTML = questions[nextQuestionId].a;
   choice1.setAttribute("data-id", "1");
-  if (questions[nextquestion].win == "a") {
+  if (questions[nextQuestionId].win == "a") {
     choice1.setAttribute("data-win", "true");
   }
   main.appendChild(choice1);
 
   var choice2 = document.createElement("a");
-  choice2.innerHTML = questions[nextquestion].b;
+  choice2.innerHTML = questions[nextQuestionId].b;
   choice2.setAttribute("data-id", "2");
-  if (questions[nextquestion].win == "b") {
+  if (questions[nextQuestionId].win == "b") {
     choice2.setAttribute("data-win", "true");
   }
   main.appendChild(choice2);
 
   var choice3 = document.createElement("a");
-  choice3.innerHTML = questions[nextquestion].c;
+  choice3.innerHTML = questions[nextQuestionId].c;
   choice3.setAttribute("data-id", "3");
-  if (questions[nextquestion].win == "c") {
+  if (questions[nextQuestionId].win == "c") {
     choice3.setAttribute("data-win", "true");
-    
+
   }
   main.appendChild(choice3);
 
   var choice4 = document.createElement("a");
-  choice4.innerHTML = questions[nextquestion].d;
+  choice4.innerHTML = questions[nextQuestionId].d;
   choice4.setAttribute("data-id", "4");
-  if (questions[nextquestion].win == "d") {
-    choice1.setAttribute("data-win", "true");
+  if (questions[nextQuestionId].win == "d") {
+    choice4.setAttribute("data-win", "true");
   }
   main.appendChild(choice4);
+  window.nextQuestionId++;
 }
 
 // Timer
 timerBox.addEventListener("click", function (event) {
 
-  var time = 31;
+  time = 31;
   var timer = setInterval(function () {
     timerBox.innerHTML = "";
     time--;
@@ -57,21 +61,44 @@ timerBox.addEventListener("click", function (event) {
       clearInterval(timer);
       timerBox.innerHTML = "<button id='start'>Play again</button>";
       question.innerHTML = "Game Over";
-      main.innerHTML = "Enter your name : ";
+      enterscore();
     }
   }, 1000);
 
-  nextQuestion("0");
-
+  nextQuestion(nextQuestionId);
+  
 });
+
+// High score
+function highScore(){
+  
+
+}
+
+// Endgame
+function enterscore(){
+  main.innerHTML = "Your Score is : " + score + "<br> Enter your name :";
+  console.log(storedScore);
+  store = storedScore.push(score);
+  localStorage.setItem("score", JSON.stringify(store));
+  score = 0;
+  nextQuestionId = 0;
+}
 
 // Check the result selected
 main.addEventListener("click", function (event) {
   win = event.target.getAttribute("data-win");
-  if(win){
+  if (win) {
     score += 10;
-  }else{
+  } else {
     time -= 5;
   }
-  console.log(score);
+  if(nextQuestionId < 10){
+    nextQuestion(nextQuestionId);
+  }else{
+    enterscore();
+  }
+
 });
+
+highScore();
